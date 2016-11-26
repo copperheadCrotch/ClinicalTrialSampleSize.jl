94c9263094e78a8bd218cf12cba1b56201eb2ba4
Zhenning Yu <yuzhenning.bio@gmail.com>
2016-11-26 09:40:28 -0500

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,16 +1,7 @@
 using Documenter
 using ClinicalTrialSampleSize
 
-makedocs(
-    modules = [ClinicalTrialSampleSize],
-    sitename = "ClinicalTrialSampleSize.jl",
-    pages = Any[
-        "Home" => "index.md",
-        "Usage" => "man/usage.md",
-        "Commands" => "man/command.md"
-    ],
-    doctest = false
-)
+makedocs()
 
 deploydocs(
     repo = "https://github.com/ContaTP/ClinicalTrialSampleSize.jl",
