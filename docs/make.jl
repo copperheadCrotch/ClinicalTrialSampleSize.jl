8e7ff8e17583323adbf5fca25ea5452fb6c729f3
Zhenning Yu <ContaTP@users.noreply.github.com>
2016-11-26 10:53:54 -0500

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,20 +1,10 @@
 using Documenter
 using ClinicalTrialSampleSize
 
-makedocs(
-    modules = [ClinicalTrialSampleSize],
-    sitename = "ClinicalTrialSampleSize.jl",
-    pages = Any[
-        "Home" => "index.md",
-        "Usage" => "man/usage.md",
-        "Commands" => "man/command.md"
-    ],
-    doctest = false
-)
+makedocs()
 
 deploydocs(
     repo = "github.com/ContaTP/ClinicalTrialSampleSize.jl.git",
-    target = "build",
     julia  = "0.5",
     deps = Deps.pip("mkdocs", "python-markdown-math"),
     make = nothing,
