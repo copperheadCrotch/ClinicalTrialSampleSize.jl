5b977df1d90a5ca9f4fa1b2164035da70799e22d
Zhenning Yu <yuzhenning.bio@gmail.com>
2016-12-04 11:51:26 -0500

    add test

--- test/TwoSampleTest.jl
+++ test/TwoSampleTest.jl
@@ -10,8 +10,11 @@ params = (
     (TwoSampleMean(3, 2, 1, true), 16.722, 0.8, 1, 0.05, "two"),
     (TwoSampleMean(10, 7, 2), 43.780, 0.9, 5, 0.05, "two"),
     (TwoSampleMean(7, 4, 1, true), 13.842, 0.9, 3, 0.1, "one"),
+    (TwoSampleMean(7, 4, 1), 13.139, 0.9, 3, 0.1, "one"),
     (TwoSampleMean(50, 45, 1.5), 137.123, 0.85, (10.4, 15.2), 0.01, "one"),
+    (TwoSampleMean(50, 45, 1.5, true), 138.142, 0.85, (10.4, 15.2), 0.01, "one"),
     (TwoSampleMean(120, 114, 1), 171.548, 0.95, (13, 17.5), 0.05, "two"),
+    (TwoSampleMean(120, 114, 1, true), 172.634, 0.95, (13, 17.5), 0.05, "two"),
 )
 
 for (test, n, p, std, alpha, side) in params
@@ -38,9 +41,11 @@ params = (
     (TwoSampleMeanInferior(2, 2, 1, -1, true), 13.170, 0.8, 1, 0.05),
     (TwoSampleMeanInferior(1.9, 2.5, 2, -1), 16.850, 0.85, 0.5, 0.05),
     (TwoSampleMeanInferior(1.94, 2.0, 1, -2.3), 46.286, 0.95, (1.1, 4.5), 0.05),
+    (TwoSampleMeanInferior(1.94, 2.0, 1, -2.3, true), 47.227, 0.95, (1.1, 4.5), 0.05),
     (TwoSampleMeanSuperior(2.5, 2, 2, 0), 45.710, 0.8, 1.3, 0.1),
     (TwoSampleMeanSuperior(20, 15, 1, 3, true), 81.810, 0.85, 5.5, 0.1),
     (TwoSampleMeanSuperior(121, 119, 1.5, 2.3), 8278.386, 0.95, (3, 9), 0.1),
+    (TwoSampleMeanSuperior(121, 119, 1.5, 2.3, true), 8278.324, 0.95, (3, 9), 0.1),
 )
 
 for (test, n, p, std, alpha) in params
@@ -64,7 +69,7 @@ params = (
     (TwoSampleMeanEqual(2, 2, 1, 0.3), 190.308, 0.8, 1, 0.05),
     (TwoSampleMeanEqual(1.94, 1.95, 2, 0, true), 8919.364, 0.85, 0.25, 0.05),
     (TwoSampleMeanEqual(112, 110, 1, 1.7), 15542.81, 0.95, (5.2, 10.3), 0.1),
-    (TwoSampleMeanEqual(59, 54, 2, 3), 23.404, 0.8, (4, 2.5), 0.1),
+    (TwoSampleMeanEqual(59, 54, 2, 3, true), 23.843, 0.8, (4, 2.5), 0.1),
 )
 
 for (test, n, p, std, alpha) in params
